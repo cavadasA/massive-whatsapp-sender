3f0e883d3789ced3de2666bc1d3ef15f2c8356f5
Antonio Cavadas <antonio.cavadas.gu@gmail.com>
2022-06-07 22:17:47 +0200

    alignment improvements for instructions

--- src/main.jsx
+++ src/main.jsx
@@ -118,59 +118,82 @@ export default function Main() {
                 <h1>Whatssive</h1>
             </div>
             <div className='row'>
-                <div className="col px-4 py-4">
-                    <div className='border border-dark textAreaBox mt-4 messageText'>
-                        <div className='row'>
-                            <div className="input-group input-group-lg">
-                                <textarea id='messageTextArea' placeholder='Pon tu mensaje aquí' style={{ border: "none", outline: "none", width: "100%" }} className="p-2 textAreaBox" rows={10} value={message} onChange={(event) => setMessage(event.target.value)}></textarea>
-                                { }
-                            </div>
-
-                        </div>
-                        <div className='row'>
-                            <div className='col-1 ms-2'>
-                                <FiTrash2 onClick={() => handleDelete("message")} className='mb-3' style={{ cursor: "pointer" }} size={30} />
-                            </div>
-                            <div className='col-1 '>
-                                <BiBold onMouseDown={() => handleTextModification('bold')} className='mb-3' style={{ cursor: "pointer" }} size={30} />
-                            </div>
-                            <div className='col-1'>
-                                <FiItalic onMouseDown={() => handleTextModification('italic')} className='mb-3' style={{ cursor: "pointer" }} size={30} />
-                            </div>
-                            {/*<div className='col-1 align-self-start'>
+                <div className='col px-4 py-4'>
+                    <div className='row'>
+                        <div className='col'>
+                            <div className='border border-dark textAreaBox mt-4 messageText'>
+                                <div className='row'>
+                                    <div className="input-group input-group-lg">
+                                        <textarea id='messageTextArea' placeholder='Pon tu mensaje aquí' style={{ border: "none", outline: "none", width: "100%" }} className="p-2 textAreaBox" rows={10} value={message} onChange={(event) => setMessage(event.target.value)}></textarea>
+                                    </div>
+                                </div>
+                                <div className='row'>
+                                    <div className='col-1 ms-2'>
+                                        <FiTrash2 onClick={() => handleDelete("message")} className='mb-3' style={{ cursor: "pointer" }} size={30} />
+                                    </div>
+                                    <div className='col-1 '>
+                                        <BiBold onMouseDown={() => handleTextModification('bold')} className='mb-3' style={{ cursor: "pointer" }} size={30} />
+                                    </div>
+                                    <div className='col-1'>
+                                        <FiItalic onMouseDown={() => handleTextModification('italic')} className='mb-3' style={{ cursor: "pointer" }} size={30} />
+                                    </div>
+                                    {/*<div className='col-1 align-self-start'>
                                 <BsEmojiSmile onClick={() => {
                                     setShowPicker(val => !val);
                                 }}
                                     className='mb-3' style={{ cursor: "pointer" }} size={30} />
                             </div>*/}
-                            <div className='col text-end me-2'>
-                                <IoMdArrowRoundForward onMouseDown={() => handlePhones(preProcessedPhones)} className='mb-3' style={{ cursor: "pointer" }} size={30} />
-                            </div>
-                            {/*showPicker && <Picker
+                                    <div className='col text-end me-2'>
+                                        <IoMdArrowRoundForward onMouseDown={() => handlePhones(preProcessedPhones)} className='mb-3' style={{ cursor: "pointer" }} size={30} />
+                                    </div>
+                                    {/*showPicker && <Picker
                                 pickerStyle={{ width: '100%' }}
                         onEmojiClick={onEmojiClick} />*/}
-                        </div>
-                    </div>
-                    <div className='border border-dark textAreaBox mt-4 telephoneNumbers'>
-                        <div className='row'>
-                            \<div className='col'>
-
-                                <div className="input-group input-group-lg">
-                                    <textarea id='phonesTextArea' placeholder='Números telefónicos' style={{ border: "none", outline: "none", width: "100%" }} className="p-2 textAreaBox" rows={5} value={preProcessedPhones} onChange={(event) => setPreProcessedPhones(event.target.value)}></textarea>
                                 </div>
                             </div>
                         </div>
-                        <div className='row'>
-                            <div className='col-1 ms-2'>
-                                <FiTrash2 onClick={() => handleDelete("phones")} className='mb-3' style={{ cursor: "pointer" }} size={30} />
+                        {whatsappLinks.length === 0 ? (
+                            <div className='col mt-4'>
+                                <h2>Instrucciones</h2>
+                                <p>En la primera caja deberás escribir el mensaje que quieras enviar a tus contactos.</p>
+                                <p>Puedes usar el botón de <b>B</b> para que el texto seleccionado se envíe en formato de negrita en whatsapp.</p>
+                                <p>También puedes usar el botón de <b><i>I</i></b> para que el texto que selecciones se envíe en formato de cursiva en whatsapp.</p>
+                                <br />
+                                <p>Una vez que hayas escrito tu mensaje e ingresado los teléfonos, podrás dar click en el botón de <b><IoMdArrowRoundForward></IoMdArrowRoundForward></b> para generar los enlaces de whatsapp.</p>
                             </div>
-                            <div className='col' style={{ cursor: "text" }} onClick={() => { handleClickOutsideTextArea('phonesTextArea') }}></div>
-                        </div>
+                        ) : ''}
+                    </div>
+                    <div className='row'>
+                        <div className='col'>
+                            <div className='border border-dark textAreaBox mt-4 telephoneNumbers'>
+                                <div className='row'>
+                                    <div className='col'>
+                                        <div className="input-group input-group-lg">
+                                            <textarea id='phonesTextArea' placeholder='Números telefónicos' style={{ border: "none", outline: "none", width: "100%" }} className="p-2 textAreaBox" rows={5} value={preProcessedPhones} onChange={(event) => setPreProcessedPhones(event.target.value)}></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='row'>
+                                    <div className='col-1 ms-2'>
+                                        <FiTrash2 onClick={() => handleDelete("phones")} className='mb-3' style={{ cursor: "pointer" }} size={30} />
+                                    </div>
+                                    <div className='col' style={{ cursor: "text" }} onClick={() => { handleClickOutsideTextArea('phonesTextArea') }}></div>
+                                </div>
 
+                            </div>
+                        </div>
+                        {whatsappLinks.length === 0 ? (
+                            <div className='col mt-4'>
+                                <p>En la segunda caja deberás escribir, en forma de lista, los teléfonos a donde quieres enviar tu mensaje y deberán estar en el siguiente formato: </p>
+                                <p>Código del país (+34, +593, +52, etc.) <b>+</b> teléfono. Por ejemplo: +59322505660 ó +525536017599</p>
+                                <p>También los podrás escribir así: </p>
+                                <p>2 ceros (00) <b>+</b> código del país (34, 593, 52, etc.) <b>+</b> teléfono. Por ejemplo: 0059322505660 ó 00525536017599</p>
+                            </div>
+                        ) : ''}
                     </div>
                 </div>
-                <div className='col px-4 py-4'>
-                    {whatsappLinks.length !== 0 ? (
+                {whatsappLinks.length !== 0 ? (
+                    <div className='col px-4 py-4'>
                         <table className="table">
                             <thead>
                                 <tr>
@@ -188,24 +211,8 @@ export default function Main() {
                                 })}
                             </tbody>
                         </table>
-
-                    ) : (
-                        <div>
-                            <h2>Instrucciones</h2>
-                            <p>En la primera caja deberás escribir el mensaje que quieras enviar a tus contactos.</p>
-                            <p>Puedes usar el botón de <b><i>I</i></b> para que el texto que selecciones se envíe en formato de cursiva en whatsapp.</p>
-                            <p>También puedes usar el botón de <b>B</b> para que el texto seleccionado se envíe en formato de negrita en whatsapp.</p>
-                            <br />
-                            <p>Una vez que hayas escrito tu mensaje e ingresado los teléfonos, podrás dar click en el botón de <b><IoMdArrowRoundForward></IoMdArrowRoundForward></b> para generar los enlaces de whatsapp.</p>
-                            <br />
-                            <br />
-                            <p>En la segunda caja deberás escribir, en forma de lista, los teléfonos a donde quieres enviar tu mensaje y deberán estar en el siguiente formato: </p>
-                            <p>Código del país (+34, +593, +52, etc.) <b>+</b> teléfono. Por ejemplo: +59322505660 ó +525536017599</p>
-                            <p>También los podrás escribir así: </p>
-                            <p>2 ceros (00) <b>+</b> código del país (34, 593, 52, etc.) <b>+</b> teléfono. Por ejemplo: 0059322505660 ó 00525536017599</p>
-                        </div>
-                    )}
-                </div>
+                    </div>
+                ) : ''}
             </div>
         </div>
     )
